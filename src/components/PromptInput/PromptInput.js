699d09b63e30277161b4d45f922d654544ba13b8
vikas <vikaspatil212222@gmail.com>
2023-06-17 18:08:03 +0530

    1

--- src/components/PromptInput/PromptInput.js
+++ src/components/PromptInput/PromptInput.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./PromptInput.css";
 
 const PromptInput = () => {
+
+    // controlling the rows of textarea by scrollheight
+    
+    const handleTextareaHeight = (e)=>{
+        const piInput = document.querySelector('.pi-input');
+        piInput.style.height = "auto";
+        if(piInput.scrollHeight > 145){
+
+            piInput.style.height = "110px";
+        }else{
+            piInput.style.height = (piInput.scrollHeight - 35) + "px";
+
+        }
+
+    }
+    
+
     return (
         <div className='pi-position'>
 
@@ -13,15 +30,15 @@ const PromptInput = () => {
                             <div className='pi-camera-icon'>
                                 <i class="fa-solid fa-camera"></i>
                             </div>
-                            <input type='text' className='pi-input' />
+                            <textarea onChange={(e)=>handleTextareaHeight(e)} rows='1' type='text' className='pi-input' placeholder='Enter details here...' required />
                             <div className='pi-microphone-icon'>
                                 <i class="fa-solid fa-microphone"></i>
                             </div>
-                            <div className='pi-button'>
+                            <button className='pi-button' role='button' type='submit'>
                                 <span class="material-symbols-outlined">
                                     send
                                 </span>
-                            </div>
+                            </button>
                         </div>
                     </div>
                 </form>
