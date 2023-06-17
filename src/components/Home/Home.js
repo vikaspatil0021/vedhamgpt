be8798d92461303b3a0693273d604bf425788b9f
vikas <vikaspatil212222@gmail.com>
2023-06-17 18:17:17 +0530

    1

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,11 +10,11 @@ const Home = () => {
             </div>
 
             <div>
-                <div style={{ background: "#111", height: "100vh", width: '100%', display: 'flex', justifyContent: 'center' }}>
-                    {/* <div style={{ background: "#000", height: "400px", width: '800px', margin: '0 50px', border: '1px solid #333', borderRadius: "8px" }}>
+                {/* <div style={{ background: "#111", height: "100vh", width: '100%', display: 'flex', justifyContent: 'center' }}>
+                    <div style={{ background: "#000", height: "400px", width: '800px', margin: '0 50px', border: '1px solid #333', borderRadius: "8px" }}>
                         hi
-                    </div> */}
-                </div>
+                    </div>
+                </div> */}
             </div>
             <div>
                 <PromptInput />
