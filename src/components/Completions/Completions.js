ccfbdce2277614d0ea28d7275db8debc471371fb
vikas <vikaspatil212222@gmail.com>
2023-07-17 12:01:28 +0530

    loader

--- src/components/Completions/Completions.js
+++ src/components/Completions/Completions.js
@@ -3,6 +3,7 @@ import React, { useEffect, useRef, useState } from 'react';
 
 import './Completions.css';
 import axios from 'axios';
+import { IonSpinner } from '@ionic/react';
 
 const Completions = ({ alertBoxTrigger, updatedComplations }) => {
     const token01 = localStorage.getItem("token01");
@@ -11,6 +12,7 @@ const Completions = ({ alertBoxTrigger, updatedComplations }) => {
 
     // get the completions data
     useEffect(() => {
+        comLoader("show")
         axios.get(process.env.REACT_APP_SERVER_URL + '/completions', {
             headers: {
                 "Authorization": "Bearer " + token01
@@ -18,15 +20,28 @@ const Completions = ({ alertBoxTrigger, updatedComplations }) => {
         }).then(res => {
             const com = [...res.data[0].data].reverse();
             setComData(com)
+            comLoader("hide")
+
         }).catch(err => {
             const error = err.response?.data.Error;
             console.log(error);
             if (error) {
+                comLoader("hide")
                 alertBoxTrigger(error.message);
 
             }
         })
     }, [updatedComplations]);
+
+
+    const comLoader = (action) =>{
+        const loader = document.querySelector('.com-loader');
+        if(action==="show"){
+            loader.style.opacity = 1;
+        }else{
+            loader.style.opacity = 0;
+        }
+    }
 
     return (
         <div className='com-container'>
@@ -52,6 +67,9 @@ const Completions = ({ alertBoxTrigger, updatedComplations }) => {
                         </div>
                     )
                 })}
+            </div>
+            <div className='com-loader'>
+                <IonSpinner name="lines-sharp-small" />
             </div>
         </div>
     )
